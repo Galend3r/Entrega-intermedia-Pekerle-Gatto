e12a4eef6b5247ac1a73b8df12216aa395491a97
Marcelo Pekerle Gatto <marcepgatto@gmail.com>
2022-04-12 23:11:04 -0300

    First commit

---
+++ entrega_intermedia/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from entrega_intermedia.users.models import User
+from entrega_intermedia.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
